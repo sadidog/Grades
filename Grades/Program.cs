ebd733851507d4a27fc5aaff8d7d81329f8ad5f7
tmochamer <tmoch28@gmail.com>
2017-02-21 10:51:05 -0600

    add a grade

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -13,6 +13,7 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(72);
         }
     }
 }
